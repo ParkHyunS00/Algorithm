9e032e7f359f6ce18d6f9e0b4bc423e1bdb8a5dc
ParkHyunSoo <parkhyuns00@naver.com>
2023-07-18 13:29:30 +0900

    백준 2346번 문제 풀이

---
+++ BOJ/2346_풍선 터뜨리기.java
@@ -0,0 +1,43 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Deque<Node> deque = new ArrayDeque<>();
+
+        for(int i=0; i<n; i++){
+            deque.add(new Node(i+1, sc.nextInt()));
+        }
+
+        int[] result = new int[n];
+        int readPaper = 0, idx = 0;
+
+        while (deque.size() > 1){
+            Node now = (readPaper >= 0) ? deque.pollFirst() : deque.pollLast();
+            for (int i=0; i<Math.abs(now.paper) - 1; i++){
+                if (now.paper < 0) deque.addFirst(deque.pollLast());
+                else deque.addLast(deque.pollFirst());
+            }
+
+            readPaper = now.paper;
+            result[idx++] = now.idx;
+        }
+
+        result[idx] = deque.pollLast().idx;
+
+        for (int r : result){
+            System.out.print(r + " ");
+        }
+    }
+
+    static class Node{
+        int idx, paper;
+        Node(int idx, int paper){
+            this.idx = idx;
+            this.paper = paper;
+        }
+    }
+}
